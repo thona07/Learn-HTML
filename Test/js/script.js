d0e2099257c6c910fb9048daa872118202c5cdf6
thona07 <eukthona06@gmail.com>
2015-02-20 15:17:01 +0700

    New foleder Test

---
+++ Test/js/script.js
@@ -0,0 +1,26 @@
+/**
+ *	PROJECT			:		Mobile Menu
+ *	Developer		:		Glean Team
+ *	Date			:		20-February-2015
+ */
+ 
+  $ ( function() {
+	var MobileMenu = {
+		init: function() {
+			this.toggleMenu();
+		},
+		toggleMenu: function() {
+			$('.toggle-button').click( function() {
+				$('ul.menu').toggleClass('show');
+			});
+			
+			$('ul.menu li > a').click( function() {
+				$(this).next('ul').toggleClass('show');
+			});
+		}
+	}
+	
+	$( document ).ready( function() {
+		MobileMenu.init();
+	});
+});
